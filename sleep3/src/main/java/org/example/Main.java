99d46c7328d850a63b73fda62f9f91f8476f812b
hr1p <127854647+hr1p@users.noreply.github.com>
2023-03-14 17:41:52 +0400

    начало

---
+++ sleep3/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String name=" Егор";
+        System.out.println("Этот человек не лох"+ name);
+    }
+}
